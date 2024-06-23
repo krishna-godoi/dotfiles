fcbe2a45ef0ff0b9634cd741e97d6f1f0701ef4a
krishna-godoi <kriscdesign@gmail.com>
2024-06-23 01:10:09 -0300

    moved from mini statusline to lualine

--- config/.config/nvim/lua/plugins/mini.lua
+++ config/.config/nvim/lua/plugins/mini.lua
@@ -31,14 +31,5 @@ return { -- Collection of various small independent plugins/modules
     -- - sd'   - [S]urround [D]elete [']quotes
     -- - sr)'  - [S]urround [R]eplace [)] [']
     require("mini.surround").setup()
-
-    -- TODO: Move to lualine as mini statusline doesn't allow for changing colors (what??)
-    --
-    -- Simple and easy statusline.
-    --  You could remove this setup call if you don't like it,
-    --  and try some other statusline plugin
-    local statusline = require("mini.statusline")
-    -- set use_icons to true if you have a Nerd Font
-    statusline.setup({ use_icons = vim.g.have_nerd_font })
   end,
 }
