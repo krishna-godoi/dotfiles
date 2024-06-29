106f7e6cf3138b4d16208e378118a5e7d7df528f
krishna-godoi <kriscdesign@gmail.com>
2024-06-29 12:59:55 -0300

    fix: disabled/reassigned conflicting oil keymaps

--- config/.config/nvim/lua/plugins/oil.lua
+++ config/.config/nvim/lua/plugins/oil.lua
@@ -1,13 +1,23 @@
 return {
   "stevearc/oil.nvim",
   opts = {
+    default_file_explorer = true,
+    delete_to_trash = true,
+    skip_confirm_for_simple_edits = true,
     view_options = {
       show_hidden = true,
+      is_always_hidden = function(name, _)
+        return name == ".." or name == ".git"
+      end,
     },
     keymaps = {
       ["<C-l>"] = false,
       ["<C-h>"] = false,
       ["<C-c>"] = false,
+      ["<C-p>"] = false,
+      ["<C-t>"] = false,
+      ["<leader>p"] = "actions.preview",
+      ["_"] = false,
     },
   },
 }
