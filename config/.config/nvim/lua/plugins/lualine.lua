70b52cb22e60f556ce9e892e2c274e79599c9410
krishna-godoi <kriscdesign@gmail.com>
2024-06-29 12:58:07 -0300

    feat: update lualine colors

--- config/.config/nvim/lua/plugins/lualine.lua
+++ config/.config/nvim/lua/plugins/lualine.lua
@@ -1,12 +1,21 @@
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = {
-    options = {
-      section_separators = { left = "", right = "" },
-    },
-    sections = {
-      lualine_x = { "filetype" },
-    },
-  },
+  config = function()
+    local custom_onedark = require("lualine.themes.onedark")
+
+    custom_onedark.normal.c.bg = "#1e1e21"
+
+    require("lualine").setup({
+      sections = {
+        lualine_c = { { "filename", path = 1 } },
+        lualine_x = { "filetype" },
+      },
+      options = {
+        theme = custom_onedark,
+        section_separators = { left = "", right = "" },
+        component_separators = { left = "|", right = "|" },
+      },
+    })
+  end,
 }
